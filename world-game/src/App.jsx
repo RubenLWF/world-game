5f53325fc6acb5528b698b9c22ef118d61610fdf
Ruben <ruben.lagerwerf@gmail.com>
2024-06-25 23:24:51 +0200

    Add styling

--- world-game/src/App.jsx
+++ world-game/src/App.jsx
@@ -1,25 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import FlagComponent from './components/FlagComponent';
 
 export default function App() {
-
-  const poop = () => {
-    console.log("Great Shot!");
-  }
-
-
   return (
     <>
       <FlagComponent />
-      <input type='text'></input>
-      <button onClick={poop}>
-        Test
-      </button>
     </>
   )
-}
-
-function SetFlag() {
-
 }
--- world-game/src/components/FlagComponent.jsx
+++ world-game/src/components/FlagComponent.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
+import { FaArrowRight } from 'react-icons/fa';
+
 import countries from '../assets/countries.json'
 import ReactCountryFlag from "react-country-flag";
 
 var i = 0
 
-shuffle(countries);
+Shuffle(countries);
 
 export default function FlagComponent() {
 
@@ -13,12 +15,13 @@ export default function FlagComponent() {
 
     function nextFlag() {
         i += 1
-        setCountry({alpha2: countries[i].alpha2, name: countries[i].name})
+        setCountry({ alpha2: countries[i].alpha2, name: countries[i].name })
     }
 
     return (
-        <>
+        <div className="flag">
             <ReactCountryFlag
+                className="flag--img"
                 countryCode={Country.alpha2}
                 svg
                 style={{
@@ -26,24 +29,27 @@ export default function FlagComponent() {
                     height: "30em"
                 }}
             />
-            <h1>{Country.name}</h1>
-            <button onClick={() => nextFlag()}>Next</button>
-        </>
+
+            <div className="flag--controls">
+                <input className="flag--input" type="text"></input>
+                <button className="flag--button" onClick={() => nextFlag()}><FaArrowRight /></button>
+            </div>
+        </div>
     )
 }
 
-function shuffle(array) {
+function Shuffle(array) {
     let currentIndex = array.length;
-  
+
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
     }
-  }
+}
